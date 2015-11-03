47dcad00daf9e365bace36430f45d57e604058aa
Lindy Astl <astl100@mail.chapman.edu>
2015-11-03 21:26:17 +0000

    second commit

--- LearningC/complex.c
+++ LearningC/complex.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
-typedef struct
+struct complex
 {
-	int x;
-    int y;
-}complex; 
+	float x;
+    float y;
+}; 
 
-int main() 
+float main() 
 {
-    complex a, b, c, z; 
+    struct complex a, b, c, z; 
     
     /*a = a.x + a.y;
     b = b.x + b.y;
@@ -18,34 +18,38 @@ int main()
     return 0;
 }
     
-const complex mult2(a, b)
+struct complex mult2(struct complex a, struct complex b)
     {
+        struct complex c;
         c.x = a.x*b.x - a.y*b.y;
       	c.y = a.x*b.x + a.x*b.y;
     	return c;
     }
     
-const complex square(a)
+struct complex square(struct complex a)
     {
+        struct complex b;
         b.x = a.x*a.x + a.y*a.y*(-1);
       	b.y = a.x*a.y;
     	return b;
     }
 
-const complex add2(a, b)
+struct complex add2(struct complex a, struct complex b)
     {
+        struct complex c;
     	c.x = a.x + b.x;
         c.y = a.y + b.y;
     	return c;
     }
 	
-const complex juliamap(c, z)
+struct complex juliamap(struct complex c, struct complex z)
     {
+        struct complex b;
     	b = add2(square(z), c);
     	return b;
     }
 
-const char * complex_print(z)
+const char* complex_print(struct complex z)
 	{
     	return ("z = %d + %d i \n", z.x, z.y);
 	}
