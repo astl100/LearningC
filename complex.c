1a9da9c27915b68989509e31cc034704cd8c2aec
Lindy Astl <astl100@mail.chapman.edu>
2015-11-17 21:05:12 -0800

    Complete Complex

--- complex.c
+++ complex.c
@@ -1,4 +1,11 @@
 #include <stdio.h>
+struct complex mult2(struct complex a, struct complex b);
+struct complex square(struct complex a);
+struct complex add2(struct complex a, struct complex b);
+struct complex juliamap(struct complex c, struct complex z);
+char * complex_print(struct complex z);
+char * test(struct complex a, struct complex b);
+
 
 struct complex
 {
@@ -8,7 +15,12 @@ struct complex
 
 int main() 
 {
-    struct complex a, b, c, z; 
+    struct complex a, b; 
+	a.x = 1; a.y = 1;
+	b.x = 1; b.y = 2;
+
+	char * l;
+	l = test(a, b);
     
     /*a = a.x + a.y;
     b = b.x + b.y;
@@ -23,7 +35,7 @@ struct complex mult2(struct complex a, struct complex b)
         struct complex c;
         c.x = a.x*b.x - a.y*b.y;
       	c.y = a.x*b.x + a.x*b.y;
-    	return c;
+    	return (c);
     }
     
 struct complex square(struct complex a)
@@ -54,3 +66,34 @@ char * complex_print(struct complex z)
     	printf ("z = %f + %f i \n", z.x, z.y);
     	return 0;
 	}
+
+char * test(struct complex a, struct complex b)
+{
+	printf("Initial Complex 1: ");
+	complex_print(a);
+	printf("\nInitial Complex 2: ");
+        complex_print(b);
+
+
+	struct complex c;
+	c = mult2(a, b);
+	printf("\nMultiply: ");
+	complex_print(c);
+
+	
+	struct complex d;
+	d = square(a);
+	printf ("\nSquare: ");
+	complex_print(d);
+
+	struct complex f;
+	f = add2(a, b);
+	printf ("\nAddition: ");
+	complex_print(f);
+
+	struct complex e;
+	e = juliamap(a, b);
+	printf ("\nJuliamap: ");
+	complex_print(e);
+
+}
